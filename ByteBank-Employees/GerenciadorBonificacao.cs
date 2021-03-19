0939d1648a15a56073703f819aa2707e9a2c4685
DeniseLuiz <denisesantos.luiz@usp.br>
2021-03-18 21:05:24 -0300

    bonification class

---
+++ ByteBank-Employees/GerenciadorBonificacao.cs
@@ -0,0 +1,17 @@
+﻿using ByteBank_Employees.Funcionários;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank_Employees
+{
+    public class GerenciadorBonificacao
+    {
+        public void Registrar(Funcionario funcionario)
+        {
+            funcionario.GetBonificacao();
+        } 
+    }
+}
